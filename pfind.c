ace52604abbd910257b5b8d5d50f6a082affcdc1
adam <adam@adam.adam>
2022-05-16 23:13:53 +0300

    added testers

---
+++ pfind.c
@@ -0,0 +1,16 @@
+
+
+
+
+
+/* Submitter: Adam
+ * Tel Aviv University
+ * Operating Systems, 2022A
+ ===========================
+ * Parallel File Find
+ * Compile with: gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -pthread pfind.c 
+ */
+ 
+ 
+ 
+
